ca468f060f293d1d9bd2d7f485b7e750de3c42ff
guilhermeboone <guilhermegalvaoboone@gmail.com>
2025-05-29 00:37:01 -0300

    Add files via upload

--- Veiculo.java
+++ Veiculo.java
@@ -6,9 +6,10 @@ package com.mycompany.veiculo;
 
 /**
  *
- * @author g.boone
+ * @author guilh
  */
 import java.util.Scanner;
+
 class VeiculoB {
     String marca;
     String modelo;
@@ -36,11 +37,12 @@ System.out.println ("A moto possui: "+cilindradas+" cilindradas");
 public class Veiculo {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        Carro[] carr = new Carro [10];
-        Moto[] motinha = new Moto [10];
+        boolean x = true;
+        Carro[] carr = new Carro [2];
+        Moto[] motinha = new Moto [2];
        
     do {
-    System.out.println ("Menu opções:\n 1- Cadastrar novo carro\n 2- Cadastrar nova moto\n 3- Exibir informações\n 4- Sair do programa");
+    System.out.println ("Menu opções:\n 1- Cadastrar novo carro\n 2- Cadastrar nova moto\n 3- Exibir informações de moto \n 4- Exibir informações de carro\n 5- Encerrar o programa.");
     int opcao = sc.nextInt();
     if (opcao ==1){
     for (int i=0; i<carr.length; i++){
@@ -55,7 +57,38 @@ public class Veiculo {
     System.out.println ();
     }
     }
-        }while (true);
+    if (opcao ==2){
+    for (int i=0; i<motinha.length; i++){
+    motinha[i] = new Moto ();
+    System.out.println ("Digite o nome da marca: ");
+    motinha[i].marca = sc.nextLine();
+    sc.nextLine();
+    System.out.println ("Digite o nome do modelo: ");
+    motinha[i].modelo = sc.nextLine();
+    System.out.println ("Digite a quantidade de cilindradas: ");
+    motinha[i].cilindradas = sc.nextInt();
+    System.out.println ();
+    }
+    }
+    if (opcao ==3){
+    for (int i=0; i<motinha.length; i++){
+    motinha[i].exibirInformacoes();
+    motinha[i].CilindradasMoto();
+
+    }
+    }
+    if (opcao ==4){
+    for (int i=0; i<carr.length; i++){
+    carr[i].exibirInformacoes();
+    carr[i].qtdPortas();
+    }
+    }
+    if (opcao ==5){
+    x = false;
+        System.out.println("Programa encerrado! ");
+    break;
+    }
+    
+        }while (x=true);
     }
 }
-
